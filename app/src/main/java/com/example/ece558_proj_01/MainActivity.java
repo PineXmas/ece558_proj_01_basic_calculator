476ae7fd8a193805c30821ab7f403c913bcff2f4
thong <thong.doan@intel.com>
2021-01-26 22:08:50 -0800

    Done and add APK

--- app/src/main/java/com/example/ece558_proj_01/MainActivity.java
+++ app/src/main/java/com/example/ece558_proj_01/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,33 +17,184 @@ public class MainActivity extends AppCompatActivity {
     // editable for operand 2
     EditText edit_op_2;
 
+    // text-view for result
+    TextView tv_result;
+
     // button for plus
     Button btn_plus;
+
+    // button for minus
+    Button btn_minus;
+
+    // button for multiply
+    Button btn_multiply;
+
+    // button for divide
+    Button btn_divide;
+
+    // button for percentage
+    Button btn_percentage;
+
+    // button for square-root
+    Button btn_sqrt;
+
+    // store value of operand 1
+    double op_1;
+
+    // store value of operand 2
+    double op_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // connect object to views
+        // connect objects to views
         edit_op_1 = findViewById(R.id.edit_op_1);
         edit_op_2 = findViewById(R.id.edit_op_2);
+        tv_result = findViewById(R.id.tv_result);
         btn_plus = findViewById(R.id.btn_plus);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_multiply = findViewById(R.id.btn_multiply);
+        btn_divide = findViewById(R.id.btn_divide);
+        btn_percentage = findViewById(R.id.btn_percentage);
+        btn_sqrt = findViewById(R.id.btn_sqrt);
     }
 
     /**
      * Handle click-event for button plus
      */
     public void onClick_btn_plus(View view){
-        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
+        // check
+        if (!check_update_operands()){
+            return;
+        }
+
+        // compute & display
+        Double result = op_1 + op_2;
+        tv_result.setText(result.toString());
     }
 
     /**
-     * Check & retrieve 2 operands
+     * Handle click-event for button minus
+     */
+    public void onClick_btn_minus(View view){
+        // check
+        if (!check_update_operands()){
+            return;
+        }
+
+        // compute & display
+        Double result = op_1 - op_2;
+        tv_result.setText(result.toString());
+    }
+
+    /**
+     * Handle click-event for button multiply
+     */
+    public void onClick_btn_multiply(View view){
+        // check
+        if (!check_update_operands()){
+            return;
+        }
+
+        // compute & display
+        Double result = op_1 * op_2;
+        tv_result.setText(result.toString());
+    }
+
+    /**
+     * Handle click-event for button divide
+     */
+    public void onClick_btn_divide(View view){
+        // check
+        if (!check_update_operands()){
+            return;
+        }
+        if (op_2 == 0){
+            Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // compute & display
+        Double result = op_1 / op_2;
+        tv_result.setText(result.toString());
+    }
+
+    /**
+     * Handle click-event for button percentage
+     */
+    public void onClick_btn_percentage(View view){
+        // check
+        if (!check_update_operand(true)){
+            return;
+        }
+
+        // compute & display
+        Double result = op_1 / 100.0;
+        tv_result.setText(result.toString());
+    }
+
+    /**
+     * Handle click-event for button square-root
+     */
+    public void onClick_btn_sqrt(View view){
+        // check
+        if (!check_update_operand(true)){
+            return;
+        }
+
+        // compute & display
+        Double result = Math.sqrt(op_1);
+        tv_result.setText(result.toString());
+    }
+
+    /**
+     * Check & update the given selected operand
+     * @param is_op_1   True if operand 1, False if operand 2
+     * @return TRUE if the operand is retrieved successfully, FALSE otherwise in case the operand
+     * is invalid
+     */
+    public boolean check_update_operand(boolean is_op_1){
+
+        // check empty
+        if (is_op_1 && edit_op_1.getText().toString().isEmpty()){
+            Toast.makeText(this, "Operand 1 is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if ((!is_op_1) && edit_op_2.getText().toString().isEmpty()){
+            Toast.makeText(this, "Operand 2 is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        try {
+            if (is_op_1) {
+                op_1 = Double.parseDouble(edit_op_1.getText().toString());
+            } else {
+                op_2 = Double.parseDouble(edit_op_2.getText().toString());
+            }
+        } catch (NumberFormatException e){
+            Toast.makeText(this, "Invalid format operands", Toast.LENGTH_SHORT).show();
+            return false;
+        } catch (Exception e){
+            Toast.makeText(this, "Invalid operands", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check & update 2 operands
      * @return TRUE if 2 operands are retrieved successfully, FALSE otherwise in case the operands
      * are invalid
      */
-    public boolean retrieve_operands(Double op_1, Double op_2){
-        return false;
+    public boolean check_update_operands(){
+
+        if (!check_update_operand(true)){
+            return false;
+        }
+
+        return check_update_operand(false);
     }
 }
