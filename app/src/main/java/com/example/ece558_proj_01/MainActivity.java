7bf3636f0e88e2f599e47d616e5fe8b982c0fdeb
pinexmas <pinexmas@gmail.com>
2021-01-27 17:47:53 -0800

    Add hint to operand 1 and 2

--- app/src/main/java/com/example/ece558_proj_01/MainActivity.java
+++ app/src/main/java/com/example/ece558_proj_01/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.ece558_proj_01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
